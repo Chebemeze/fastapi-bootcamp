df6884143e74322ae18c79637a7bb56b2766d5fb
Chebemeze <Chebemeze@gmail.com>
2026-07-31 15:48:14 +0100

    comment to understand code

--- python-lessons/binary_insert.py
+++ python-lessons/binary_insert.py
@@ -13,6 +13,7 @@ def insert_node(node, price):
     elif price > node.price:
         node.right = insert_node(node.right, price)
 
+#test case to test the insert function in python
 root = CupNode(4.00)
 insert_node(root, 3.00)
 insert_node(root, 5.00)
