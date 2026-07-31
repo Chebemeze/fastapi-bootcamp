4d77e8c43b5d04ec8f6f1870c3c538f818f9dbd8
Chebemeze <Chebemeze@gmail.com>
2026-07-31 15:35:55 +0100

    implementing binary insert with python

---
+++ python-lessons/binary_insert.py
@@ -0,0 +1,21 @@
+class CupNode:
+    def __init__(self, price):
+        self.price = price
+        self.left = None
+        self.right = None
+
+def insert_node(node, price):
+    if node is None:
+        return CupNode(price)
+    
+    if price < node.price:
+        node.left = insert_node(node.left, price)
+    elif price > node.price:
+        node.right = insert_node(node.right, price)
+
+root = CupNode(4.00)
+insert_node(root, 3.00)
+insert_node(root, 5.00)
+
+print(root.left.price)
+print(root.right.price)
