ad4b3fa940544044bed6d304377ee8cb2475fd40
Chebemeze <Chebemeze@gmail.com>
2026-08-26 16:41:01 +0100

    add visibility filter to ISS pass retrieval and enhance print functionality

--- python-lessons/isspass.py
+++ python-lessons/isspass.py
@@ -35,7 +35,7 @@ def get_next_passes(location: dict):
 
     lat_titude, lon_gitude = location["lat"], location["lon"]
     try:
-        response = requests.get(f"https://iss-api.polluxlabs.io/iss-pass?lat={lat_titude}&lon={lon_gitude}")
+        response = requests.get(f"https://iss-api.polluxlabs.io/iss-pass?lat={lat_titude}&lon={lon_gitude}&visible_only=true")
  
         #checks response status and catches raises exceptions for unssuccessful Http Errors
         #like 404, 401, 500 etc
@@ -59,5 +59,52 @@ def get_next_passes(location: dict):
         cleaned_pass.append(useful_data)
     return cleaned_pass
 
+def print_next_pass(pass_data: dict):
+    """This function takes a single pass_data dictionary and prints the time, duration, height the ISS will pass
+    the users entered location. It also prints if the ISS will be visible to the user and the direction they should look at
+    """
+    if not pass_data:
+        print("No upcoming ISS passes found.")
+        return
+
+    future_date= pass_data["rise_time"]
+    user_timezone = future_date.astimezone() #converts future_date to the timezone of the user
+
+    # %A: specifies the day of the week, %I: hour,
+    # %M: minutes, %p: time of the day, either AM or PM
+    # %Z: the timezone e.g WAT
+    rise_time = user_timezone.strftime("%A, %I:%M %p %Z")
+    direction = pass_data["direction"]
+    duration_min = pass_data["duration"]//60
+    height = pass_data["max_elevation"]
+
+    if height >= 45:
+        tip = f"Look {direction} — it will look like a bright, steadily moving star."
+        telescope_status = "No telescope needed!"
+    elif height >= 20:
+        tip = f"Look towards the {direction}. It climbs high enough to spot, but find a spot clear of tall trees."
+        telescope_status = "No telescope needed, but find a clear viewing area."
+    else:
+        tip = f"Visibility at {direction} is poor. It just skims above the horizon"
+        telescope_status = "It will be difficult to see unless you have an entirely flat, unobstructed horizon."
+
+    print("🔭  NEXT ISS VISIBLE PASS")
+    print(f"When: {rise_time}")
+    print(f"Duration: {duration_min} minute(s)")
+    print(f"Height: {height}° above the horizon")
+    print(f"Tip: {tip}")
+    print(telescope_status)
+
+# test case for both functions
 if __name__ == "__main__":
-    get_next_passes({"lat": 4.8242, "lon": 7.0336})
+    res = get_next_passes({"lat": 4.8242, "lon": 7.0336})
+    print(res)
+
+    libra_ry = {
+        "rise_time": datetime.fromisoformat("2026-08-28T14:18:41Z"),
+        "direction": "NNW",
+        "duration": 402,
+        "max_elevation": 70.3
+    }
+
+    print_next_pass(libra_ry)
